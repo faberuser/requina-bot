cb96aae9631e0995f3bc08b127a4ea9819471254
Faber <faber8164@gmail.com>
2021-08-15 22:35:57 +0700

    modified:   cogs/hololive_playper.py 	modified:   config.py

--- cogs/hololive_playper.py
+++ cogs/hololive_playper.py
@@ -83,6 +83,65 @@ class Hololive(commands.Cog):
                 pass
             self.player.play(audio)
 
+    async def hololive_(self):
+        try:
+            # get music songs path
+            midnight = []
+            for file in os.listdir('./hololive chill/Midnight'):
+                if file.endswith('.mp3'):
+                    midnight.append((f'./hololive chill/Midnight/{file}', file[:-4]))
+            midnight.sort()
+            sunshine = []
+            for file in os.listdir('./hololive chill/Sunshine'):
+                if file.endswith('.mp3'):
+                    sunshine.append((f'./hololive chill/Sunshine/{file}', file[:-4]))
+            sunshine.sort()
+
+            # append to a public list
+            for music in midnight:
+                self.queue.append(music)
+            for music in sunshine:
+                self.queue.append(music)
+
+            # join voice channel
+            try:
+                voice_channel = self.client.get_channel(config.hololive_channel)
+                self.player = await voice_channel.connect(reconnect=True, timeout=30)
+            except:
+                try:
+                    voice_channel = await self.client.fetch_channel(config.hololive_channel)
+                    self.player = await voice_channel.connect(reconnect=True, timeout=30)
+                except:
+                    try:
+                        voice_channel = self.client.get_channel(config.hololive_channel)
+                        self.player = await self.client.move_to(voice_channel)
+                    except:
+                        voice_channel = await self.client.get_channel(config.hololive_channel)
+                        self.player = await self.client.move_to(voice_channel)
+            self.channel = voice_channel
+
+            # loop music player
+            queue = cycle(self.queue)
+            while True:
+                if self.player == None or self.channel == None or self.queue == []:
+                    try:
+                        await self.player.disconnect(force=True)
+                    except:
+                        pass
+                    break
+                if self.player.is_playing():
+                    await sleep(3)
+                    continue
+                playing = next(queue)
+                audio = discord.FFmpegPCMAudio(playing[0])
+                try:
+                    await voice_channel.edit(name=f"{playing[1]} - Hololive Chill (24/7)")
+                except:
+                    pass
+                self.player.play(audio)
+        except:
+            traceback.print_exc()
+
     @commands.command()
     async def hololive_list(self, ctx):
         midnight = []
