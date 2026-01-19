78d7a832794868948a30e2ae13a763e1c46564ae
khoile <faber8164@gmail.com>
2026-01-19 21:05:43 +0700

    update gemini flash to latest

--- cogs/genai.py
+++ cogs/genai.py
@@ -8,6 +8,7 @@ from discord.ext import commands
 load_dotenv()
 client = genai.Client(api_key=os.getenv('GENAI_KEY'))
 
+
 class GenAI(commands.Cog):
     def __init__(self, client):
         self.client = client
@@ -18,17 +19,20 @@ class GenAI(commands.Cog):
         try:
             messages = [message async for message in ctx.channel.history(limit=history_length)]
             messages.reverse()
-            text = "\n".join([msg.author.display_name + ": " + msg.content for msg in messages if msg.content and not msg.author.bot and not msg.content.lower().startswith("r.")])
+            text = "\n".join([msg.author.display_name + ": " +
+                             msg.content for msg in messages if msg.content and not msg.author.bot and not msg.content.lower().startswith("r.")])
             if not text:
                 await bot_msg.edit(content="No messages to summarize.")
                 return
             response = client.models.generate_content(
-                model="gemini-2.0-flash", contents="Tóm tắt cuộc trò chuyện này:" + "\n\n" + text
+                model="gemini-flash-latest", contents="Tóm tắt cuộc trò chuyện này:" + "\n\n" + text
             )
-            embed = discord.Embed(title=f"Summary for Last {str(history_length)} Messages", description=response.text, color=config.embed_color)
+            embed = discord.Embed(
+                title=f"Summary for Last {str(history_length)} Messages", description=response.text, color=config.embed_color)
             await bot_msg.edit(content="", embed=embed)
         except Exception as e:
             await bot_msg.edit(content=f"An error occurred: {e}")
+
 
 async def setup(client):
     await client.add_cog(GenAI(client))
